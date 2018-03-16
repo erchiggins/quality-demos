30d43fc19f2c13e1fc5c1891f0032a72446f6840
erchiggins <emilyrchiggins@gmail.com>
2018-03-16 13:20:57 -0400

    more controllers

--- RestServiceDemo/src/main/java/com/revature/beans/Surfer.java
+++ RestServiceDemo/src/main/java/com/revature/beans/Surfer.java
@@ -1,24 +1,35 @@
 package com.revature.beans;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Surfer {
 	
 	public Surfer() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Surfer(String name, int numOfBoards) {
+	public Surfer(int id, String name, int numOfBoards) {
 		super();
+		this.id = id;
 		this.name = name;
 		this.numOfBoards = numOfBoards;
 	}
+	
+	private int id;
 	private String name;
 	private int numOfBoards;
+	
+	@XmlElement 
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	@XmlElement
 	public int getNumOfBoards() {
 		return numOfBoards;
 	}
@@ -28,6 +39,13 @@ public class Surfer {
 	@Override
 	public String toString() {
 		return "Surfer [name=" + name + ", numOfBoards=" + numOfBoards + "]";
+	}
+	@XmlAttribute
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
--- RestServiceDemo/src/main/java/com/revature/controller/SurferController.java
+++ RestServiceDemo/src/main/java/com/revature/controller/SurferController.java
@@ -6,19 +6,32 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.beans.Surfer;
 
-@RestController 
+@RestController
 public class SurferController {
-	
-	@GetMapping("/surfer/all")
-	public ResponseEntity<List<Surfer>> getAllSurfers(){
+
+	@GetMapping("/allSurfers")
+	public ResponseEntity<List<Surfer>> getAllSurfers() {
 		List<Surfer> surfList = new ArrayList<>();
-		surfList.add(new Surfer("Cool Joe",8));
-		surfList.add(new Surfer("Angela Merkel",300));
-		return new ResponseEntity<>(surfList,HttpStatus.OK);
+		surfList.add(new Surfer(1,"Cool Joe", 8));
+		surfList.add(new Surfer(2,"Angela Merkel", 300));
+		return new ResponseEntity<>(surfList, HttpStatus.OK);
+	}
+
+	//ResponseEntity is not necessary, but it's a convenient wrapper
+	@RequestMapping(value = "/surferJSON", method = RequestMethod.GET, produces = "application/json")
+	public Surfer getSurferJSON() {
+		return new Surfer(3, "Jason", 2);
+	}
+
+	@RequestMapping(value = "/surferXML", method = RequestMethod.GET, produces = "application/xml")
+	public Surfer getSurferXml() {
+		return new Surfer(4, "Xavier", 3);
 	}
 
 }
