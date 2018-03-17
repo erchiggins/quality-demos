c0af0ff55c6c94db78b3acb2725da5ec03532f94
erchiggins <emilyrchiggins@gmail.com>
2018-03-17 12:23:47 -0400

    Rest Client solution

--- RestClientDemo/src/main/java/com/revature/client/SurferClient.java
+++ RestClientDemo/src/main/java/com/revature/client/SurferClient.java
@@ -1,14 +1,42 @@
 package com.revature.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.revature.beans.Surfer;
 
 @Component("surferClient")
 public class SurferClient {
-	
+
 	private String resourceUrl;
-	
-	public void setResourceUrl(String resourceUrl){
+
+	public void setResourceUrl(String resourceUrl) {
 		this.resourceUrl = resourceUrl;
+	}
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	// Jackson is doing a lot of work behind the scenes
+	// We can use the .exchange method instead of getForEntity to achieve finer-grained control of the request being sent 
+	public List<Surfer> getSurfers() {
+		ResponseEntity<List<Surfer>> response = this.restTemplate.exchange(this.resourceUrl + "/allSurfers",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<Surfer>>() {
+				});
+		return response.getBody();
+	}
+
+	public Surfer addSurfer(Surfer Surfer) {
+		HttpEntity<Surfer> request = new HttpEntity<Surfer>(Surfer);
+		return this.restTemplate.postForObject(this.resourceUrl + "/addSurfer", request, Surfer.class);
 	}
 
 }
--- RestClientDemo/src/main/java/com/revature/main/Driver.java
+++ RestClientDemo/src/main/java/com/revature/main/Driver.java
@@ -1,10 +1,33 @@
 package com.revature.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.revature.beans.Surfer;
+import com.revature.client.SurferClient;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		SurferClient sc = ac.getBean("surferClient", SurferClient.class);
+		
+		//set this as the URI of the service you're attempting to consume
+		//(and make sure it's running before you run this class) 
+		sc.setResourceUrl("http://localhost:8087");
+		
+		//System.out.println(sc.getSurfers());
+		
+		//iterate through the surfers retrieved from the /allSurfers endpoint
+		//Jackson has converted the JSON-formatted string into Java objects
+		for(Surfer s1 : sc.getSurfers()){
+			System.out.println(s1);
+		}
+		
+		//now create a surfer object and POST it back to the service 
+		Surfer s2 = new Surfer(5,"Crazy Chad",4);
+		sc.addSurfer(s2);
 	}
 
 }
